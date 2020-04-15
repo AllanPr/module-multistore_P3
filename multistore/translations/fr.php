beb4d5cd2814d42c7ec5fb1787f07e5691c46c43
AllanPr <58980399+AllanPr@users.noreply.github.com>
2020-04-15 08:49:58 +0200

    Ajout fr translation
    ajout du fichier des trads

---
+++ multistore/translations/fr.php
@@ -0,0 +1,23 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{multistore}prestashop>multistore_65e22de9681f419e85ac4faeb800f154'] = 'Vous aide à éditer les facture en choisissant le magasin';
+$_MODULE['<{multistore}prestashop>multistore_d6f47e627cb8b9186caa435aba1c32ae'] = 'Êtes vous sûr de vouloir supprimer ce module ?';
+$_MODULE['<{multistore}prestashop>multistore_c441ee50c88f6e5e3a7c69fc69dad8b8'] = 'Commandes';
+$_MODULE['<{multistore}prestashop>multistore_f7f7f8c365a50fb672efc10713444157'] = 'Commandes du magasin';
+$_MODULE['<{multistore}prestashop>multistore_7acd148468a59641357e7ea01a28deec'] = 'Récupération au magasin';
+$_MODULE['<{multistore}prestashop>multistore_f65fa6c7878c2a6aa783c30cd4643401'] = 'Récupération au magasin gratuite';
+$_MODULE['<{multistore}prestashop>adminmultistoreshopcmdcontroller_8ad75c5a8821cc294f189181722acb56'] = 'Prénom';
+$_MODULE['<{multistore}prestashop>adminmultistoreshopcmdcontroller_342f5c77ed008542e78094607ce1f7f3'] = 'Nom';
+$_MODULE['<{multistore}prestashop>configure_3225a10b07f1580f10dee4abc3779e6c'] = 'Paramètres';
+$_MODULE['<{multistore}prestashop>configure_a8fb1529e16c230ba1a97e14a5d981b4'] = 'Gestion du magasin';
+$_MODULE['<{multistore}prestashop>configure_34b6cd75171affba6957e308dcbd92be'] = 'Version';
+$_MODULE['<{multistore}prestashop>parameter_b7d59aef402bcc22823fb6b1e71e2dd6'] = 'Paramètres généraux';
+$_MODULE['<{multistore}prestashop>parameter_75b9a26dc32dde1ec79750acb3981488'] = 'Voici la page de configuration du module. Une fois configuré il facilitera votre travail';
+$_MODULE['<{multistore}prestashop>store_management_a8fb1529e16c230ba1a97e14a5d981b4'] = 'Gestion du magasin';
+$_MODULE['<{multistore}prestashop>store_management_fb54f3c5992b96d001bb16e8e92d968d'] = 'Magasin';
+$_MODULE['<{multistore}prestashop>store_management_fa5473530e4d1a5a1e1eb53d2fedb10c'] = 'Employé';
+$_MODULE['<{multistore}prestashop>store_management_c9cc8cce247e49bae79f15173ce97354'] = 'Sauvegarder';
+$_MODULE['<{multistore}prestashop>displaycarrierextracontent_e6a292351684d97fef6a0e8b0dfdc701'] = 'Choisissez un magasin pour récupérer votre commande';
+$_MODULE['<{multistore}prestashop>displaycarrierextracontent_5dbe1ffd37901b1c1af30d79a1a6281e'] = 'Choisissez ce magasin';
